1199d3129c4c4825709c1d1e0b476b7b1c1ef410
Riley Rangel <rrangel91@gmail.com>
2017-11-03 14:52:45 -0700

    Adjust styling to make more visually appealing. #11

--- client/todo-form.js
+++ client/todo-form.js
@@ -3,27 +3,33 @@ import React from 'react'
 export default function TodoForm({ handleSubmit }) {
   return (
     <div className='row' id='todo-form'>
-      <form onSubmit={ handleSubmit } className='col s8 push-s2 card'>
-        <div className='row'>
-          <div className='input-field col s5 push-s1'>
-            <h5 htmlFor='task'>New Item: </h5>
-            <input
-              placeholder='What to do...'
-              id='task'
-              name='task'
-              type='text' />
+      <form onSubmit={ handleSubmit } className='col s8 push-s2'>
+        <div className='card'>
+          <div className='row'>
+            <div className='input-field col s5 push-s1'>
+              <h5 htmlFor='task'>New Item: </h5>
+              <input
+                placeholder='What to do...'
+                id='task'
+                name='task'
+                type='text' />
+            </div>
+            <div className='input-field col s5 push-s1 left'>
+              <h5 htmlFor='task'>Due Date: </h5>
+              <input
+                placeholder='When to do...'
+                id='due'
+                name='due'
+                type='date' />
+            </div>
           </div>
-          <div className='input-field col s5 push-s1 left'>
-            <h5 htmlFor='task'>Due Date: </h5>
-            <input
-              placeholder='When to do...'
-              id='due'
-              name='due'
-              type='date' />
+          <div className='row'>
+            <div className='card-action' style={{ 'overflow': 'auto' }}>
+              <div className='col s1 push-s5'>
+                <button className='btn' type='submit'>Save</button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className='row'>
-          <button className='btn col s2 push-s5'>Save</button>
         </div>
       </form>
     </div>
--- client/todo-list.js
+++ client/todo-list.js
@@ -3,8 +3,10 @@ import React from 'react'
 export default function TodoList({ todos }) {
   return (
     <div className='row'>
-      <div className='col s10 push-s1'>
-        { todos.map((todo, index) => ListItem(todo, index)) }
+      <div className='col s8 push-s2'>
+        <div className='row'>
+          { todos.map((todo, index) => ListItem(todo, index)) }
+        </div>
       </div>
     </div>
   )
@@ -12,9 +14,13 @@ export default function TodoList({ todos }) {
 
 function ListItem({ task, due }, index) {
   return (
-    <div key={ index } className='card col s3 left todo'>
-      <h5>{ task }</h5>
-      <span>Due by: { due }</span>
+    <div key={ index } className='col s4 left todo'>
+      <div className='card' style={{ 'height': '10rem' }}>
+        <div className='card-content center-align'>
+          <h5>{ task }</h5>
+          <span>Due by: { due }</span>
+        </div>
+      </div>
     </div>
   )
 }
