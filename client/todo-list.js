8828b08871d7e099b8afbc89349e651bcb3a851a
Riley Rangel <rrangel91@gmail.com>
2017-11-04 15:12:10 -0700

    Implementing improvements per code review. #11

--- client/todo-list.js
+++ client/todo-list.js
@@ -5,14 +5,14 @@ export default function TodoList({ todos }) {
     <div className='row'>
       <div className='col xl8 l8 m12 s12 push-xl2 push-l2'>
         <div className='row'>
-          { todos.map((todo, index) => ListItem(todo, index)) }
+          { todos.map((todo, index) => renderListItem(todo, index)) }
         </div>
       </div>
     </div>
   )
 }
 
-function ListItem({ task, due }, index) {
+function renderListItem({ task, due }, index) {
   return (
     <div key={ index } className='col xl4 l4 m6 s12 left todo'>
       <div className='card blue-grey darken-1' style={{ 'height': '10rem' }}>
